588ae696130887df23acfca8eea3ed6868ab4460
Shyaboi <ian.shane.sears@gmail.com>
2020-07-06 23:20:15 -0700

    added api and api output

--- ok.js
+++ ok.js
@@ -1,3 +1,4 @@
+// input word variable
 const ward = ''
 document.getElementById('strInput').onkeydown = function(e){
     if(e.keyCode == 13){
@@ -5,13 +6,21 @@ document.getElementById('strInput').onkeydown = function(e){
       
       console.log(ward)
       rev()
+      api()
       return ward
     }
  };
 
+function api(){
+    let ward = document.getElementById('strInput').value
+    fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${ward}?key=87734587-fa53-4551-a401-d89710c37236`)
+    .then(response => response.json())
+    // .then(data => console.log(data[0].meta.syns))
+    .then(data => document.getElementById('apiSyn').innerHTML = data[0].meta.syns);
+    
+}
 
-
-function rev() {
+ function rev() {
     let ward = document.getElementById('strInput').value
     console.log(ward)
     const word = ward
